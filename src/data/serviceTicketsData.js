2eba7b64814247ce2f86404bf3058917dd30bd17
Taylor Seager <taylorseager718@gmail.com>
2024-07-29 21:12:13 -0500

    added CORS config, now tickets are showing up

--- src/data/serviceTicketsData.js
+++ src/data/serviceTicketsData.js
@@ -1,7 +1,7 @@
-const _apiUrl = "/api/servicetickets";
+const baseUrl = "http://localhost:5000"
 
 export const getServiceTickets = () => {
-  return fetch(_apiUrl).then((r) => r.json());
+  return fetch(baseUrl+"/servicetickets").then((r) => r.json());
 };
 
 //export a function here that gets a ticket by id
