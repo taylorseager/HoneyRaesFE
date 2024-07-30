68bb40785f893acb9b03bdddd3818635f725a2d0
Taylor Seager <taylorseager718@gmail.com>
2024-07-29 19:41:04 -0500

    updated defineConfig target localhost

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig(() => {
       open: true,
       proxy: {
         "/api": {
-          target: "https://localhost:5001",
+          target: "https://localhost:5092",
           changeOrigin: true,
           secure: false,
         },
