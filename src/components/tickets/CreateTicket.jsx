c0ce78572f3ee6c7399333b21431d3067c7c1b1f
Taylor Seager <taylorseager718@gmail.com>
2024-08-03 10:36:56 -0500

    save point

--- src/components/tickets/CreateTicket.jsx
+++ src/components/tickets/CreateTicket.jsx
@@ -1,3 +1,53 @@
+import { Form } from "reactstrap";
+
+const initialState = {
+  customerId: '',
+  employeeId: '',
+  description: '',
+  emergency: false,
+  dateCompleted: null,
+};
+
 export default function CreateTicket() {
-  return <h3>Submit a Ticket</h3>;
+  <Form>
+    <FormGroup>
+          <Label for="Customer Name">
+            Customer Name
+          </Label>
+          <Input
+            id="customerId"
+            name="Customer Name"
+            value={formInput.customerId}
+            placeholder="Enter Customer Name"
+            type="textarea"
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="Employee Name">
+            Employee Name
+          </Label>
+          <Input
+            id="employeeId"
+            name="Employee Name"
+            value={formInput.employeeId}
+            placeholder="Enter Employee Name"
+            type="textarea"
+            onChange={handleChange}
+          />
+        </FormGroup>
+    <FormGroup>
+          <Label for="description">
+            Description
+          </Label>
+          <Input
+            id="description"
+            name="description"
+            value={formInput.description}
+            placeholder="Describe your issue"
+            type="textarea"
+            onChange={handleChange}
+          />
+        </FormGroup>
+  </Form>
 }
