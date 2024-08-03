59564d45489dae9aebf995fb88aa2e01b3c37e35
Taylor Seager <taylorseager718@gmail.com>
2024-08-03 11:18:06 -0500

    form working, however need to update customer and employee to be selectors

--- src/components/tickets/CreateTicket.jsx
+++ src/components/tickets/CreateTicket.jsx
@@ -1,22 +1,44 @@
-import { Form } from "reactstrap";
+import { useState } from 'react';
+import { Form, FormGroup, Input, Label, Button } from 'reactstrap';
+import { useNavigate } from 'react-router-dom';
+import { createNewServiceTicket } from '../../data/serviceTicketsData';
 
 const initialState = {
-  customerId: '',
-  employeeId: '',
+  customerId: 0,
+  employeeId: 0,
   description: '',
   emergency: false,
   dateCompleted: null,
 };
 
 export default function CreateTicket() {
-  <Form>
+  const [formInput, setFormInput] = useState(initialState);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const payload = { ...formInput };
+    createNewServiceTicket(payload).then(() =>{navigate('/tickets')});
+  };
+
+  return (
+  <Form onSubmit={handleSubmit}>
     <FormGroup>
           <Label for="Customer Name">
             Customer Name
           </Label>
           <Input
             id="customerId"
-            name="Customer Name"
+            name="customerId"
             value={formInput.customerId}
             placeholder="Enter Customer Name"
             type="textarea"
@@ -29,7 +51,7 @@ export default function CreateTicket() {
           </Label>
           <Input
             id="employeeId"
-            name="Employee Name"
+            name="employeeId"
             value={formInput.employeeId}
             placeholder="Enter Employee Name"
             type="textarea"
@@ -49,5 +71,32 @@ export default function CreateTicket() {
             onChange={handleChange}
           />
         </FormGroup>
+        <FormGroup>
+        <Label for="emergency">
+            Emergency?
+          </Label>
+          <Input
+            id="emergency"
+            name="emergency"
+            value={formInput.emergency ? 'true' : 'false'}
+            type="select"
+            onChange={(e) => {
+              const { name, value } = e.target;
+              setFormInput((prevState) => ({
+                ...prevState,
+                [name]: value === 'true',
+              }))
+            }}
+            >
+            <option value={true}>
+              Yes
+            </option>
+            <option value={false}>
+              No
+            </option>
+            </Input>
+        </FormGroup>
+        <Button type="submit">Submit</Button>
   </Form>
+  );
 }
--- src/data/serviceTicketsData.js
+++ src/data/serviceTicketsData.js
@@ -29,3 +29,16 @@ export const completeThisTicket = (id) => new Promise((resolve, reject) => {
     .then((data) => resolve((data)))
     .catch(reject);
 });
+
+export const createNewServiceTicket = (payload) => new Promise((resolve, reject) => {
+  fetch(`${baseUrl}/servicetickets/create`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+  .then((response) => response.json())
+  .then((data) => resolve((data)))
+  .catch(reject);
+});
